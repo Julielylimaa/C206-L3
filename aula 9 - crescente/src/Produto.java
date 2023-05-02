abd201402196dbc95cc0d1045475672359ece5ce
Julielylimaa <juhlima26@icloud.com>
2023-05-02 19:30:22 -0300

    main

---
+++ aula 9 - crescente/src/Produto.java
@@ -0,0 +1,24 @@
+public class Produto implements Comparable<Produto> {
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public int compareTo(Produto o) {
+        return Double.compare(this.getPreco(), o.getPreco());
+    }
+
+
+}
